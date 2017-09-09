39ca4fdc7927853b103d3599bba8c8e0baee2def
Chobits <chobits@live.cn>
2017-09-09 17:22:06 +0800

    1

--- src/directives/index.js
+++ src/directives/index.js
@@ -4,19 +4,5 @@ export const MuiInput = {
     // 当绑定元素插入到 DOM 中。
     inserted: function (el) {
         mui(el).input()
-
-
-
-
-
-        el.addEventListener('focus', function () {
-
-
-
-            
-
-
-            mui.trigger(el, 'input')
-        })
     }
 }
